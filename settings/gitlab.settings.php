971e73b23268670f17b03255f7713cfbe2253b86
flux423 <thomas.scola@acquia.com>
2021-03-23 16:23:51 -0400

    Revert to Drupal8 for Db settings

--- settings/gitlab.settings.php
+++ settings/gitlab.settings.php
@@ -2,41 +2,15 @@
 
 /**
  * @file
- * Common settings for CI envs.
+ * GitLab environment specific settings.
  */
-
-$config['system.logging']['error_level'] = 'verbose';
-
-$dir = dirname(DRUPAL_ROOT);
-$settings['file_private_path'] = $dir . '/files-private';
-$settings['trusted_host_patterns'] = [
-  '^.+$',
-];
 
 /**
- * Sensible CI defaults for databases.
+ * Overwrite CI default database host name.
  *
- * This will be overridden by system specific CI files.
+ * @see ci.settings.php
  */
-$databases['default']['default'] = [
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'host' => 'mysql',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-  'prefix' => '',
-];
-
-/**
- * Skip file system permissions hardening.
- *
- * The system module will periodically check the permissions of your site's
- * site directory to ensure that it is not writable by the website user. For
- * sites that are managed with a version control system, this can cause problems
- * when files in that directory such as settings.php are updated, because the
- * user pulling in the changes won't have permissions to modify files in the
- * directory.
- */
-$settings['skip_permissions_hardening'] = TRUE;
+$databases['default']['default']['host'] = 'mysql';
+$databases['default']['default']['password'] = 'drupal8';
+$databases['default']['default']['username'] = 'drupal8';
+$databases['default']['default']['database'] = 'drupal8';
