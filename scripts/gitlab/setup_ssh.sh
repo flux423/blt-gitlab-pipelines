b826c6083bf90b5510d7ea612804d6cbc707208e
flux423 <thomas.scola@acquia.com>
2021-03-30 08:26:41 -0400

    Revert ssh script

--- scripts/gitlab/setup_ssh.sh
+++ scripts/gitlab/setup_ssh.sh
@@ -6,11 +6,16 @@ set -ev
 
 apt-get update -y && apt-get install openssh-client -y
 eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY")
 mkdir -p ~/.ssh
-rm -rf ~/.ssh/known_hosts
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+chmod 700 ~/.ssh
 ssh-keyscan -H '$SSH_KNOWN_HOSTS' >> ~/.ssh/known_hosts
 ssh-keyscan $SSH_KNOWN_HOSTS | sort -u - ~/.ssh/known_hosts -o ~/.ssh/known_hosts
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+
+
+
 
 set +v
