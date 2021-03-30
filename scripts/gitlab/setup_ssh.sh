26bfb9d9bad536cf8fb0caf0b475f0d3de46c78f
flux423 <thomas.scola@acquia.com>
2021-03-30 08:46:01 -0400

    UPdate ssh script.

--- scripts/gitlab/setup_ssh.sh
+++ scripts/gitlab/setup_ssh.sh
@@ -6,16 +6,10 @@ set -ev
 
 apt-get update -y && apt-get install openssh-client -y
 eval $(ssh-agent -s)
+cat $SSH_PRIVATE_KEY | tr -d '\r' | ssh-add -
 mkdir -p ~/.ssh
-ssh-add <(echo "$SSH_PRIVATE_KEY")
-chmod 700 ~/.ssh
-ssh-keyscan -H '$SSH_KNOWN_HOSTS' >> ~/.ssh/known_hosts
-ssh-keyscan $SSH_KNOWN_HOSTS | sort -u - ~/.ssh/known_hosts -o ~/.ssh/known_hosts
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod -R 700 ~/.ssh
+ssh-keyscan $SSH_KNOWN_HOSTS >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
-
-
-
 
 set +v
