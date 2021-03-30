af403f50df13359ad77d15c43f5fc7db11e5ab3d
flux423 <thomas.scola@acquia.com>
2021-03-30 11:20:42 -0400

    Remove ssh script and move to ci file.

--- scripts/gitlab/deploy_artifact.sh
+++ scripts/gitlab/deploy_artifact.sh
@@ -2,6 +2,13 @@
 
 set -ev
 
-vendor/bin/blt artifact:deploy --commit-msg "Automated commit by Gitlab Pipelines for Build $CI_CONCURRENT_ID" --branch "$CI_COMMIT_BRANCH-build" --no-interaction --verbose
+# Prep date format for deploy  ******
+ date +'FORMAT'
+ date +'%m/%d/%Y'
+ date +'%r'
+ TIMESTAMP=$(date +'%m-%d-%Y')
+ echo "${TIMESTAMP}"
+ 
+vendor/bin/blt artifact:deploy --commit-msg "$CI_CONCURRENT_ID-$CI_COMMIT_MESSAGE" --branch "$CI_COMMIT_BRANCH-build" --tag release-"${TIMESTAMP}"--$CI_JOB_ID -n --ignore-dirty --no-interaction --verbose
 
 set +v
