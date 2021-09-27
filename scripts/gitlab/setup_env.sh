572c7b7de635a909494878620959d059a1c9b667
Thomas P. Scola Jr <admin@423interactive.com>
2021-09-27 11:40:57 -0400

    Update CI script and env variables.

--- scripts/gitlab/setup_env.sh
+++ scripts/gitlab/setup_env.sh
@@ -12,10 +12,15 @@ git config --global user.email "noreply@gitlab.com"
 touch /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
 echo 'memory_limit = -1' >> /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
 
-# Create a MySQL database for drupal8 to use
+# Create a MySQL database for drupal to use
 echo "CREATE DATABASE IF NOT EXISTS \`drupal\`;" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
 echo "CREATE USER 'drupal'@'%' IDENTIFIED BY 'drupal';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
 echo "GRANT ALL ON drupal.* TO 'drupal'@'%';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
 echo "FLUSH PRIVILEGES;" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
+
+# Install Google Chrome and Cleanup Binary
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 
 set +v
