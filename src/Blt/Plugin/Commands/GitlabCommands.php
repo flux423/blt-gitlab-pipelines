ff8765c83e022bae7d5dd3a0dda25fdfa53e2336
Thomas P. Scola Jr <admin@423interactive.com>
2021-09-27 11:51:37 -0400

    Update CI script and env variables again.

--- src/Blt/Plugin/Commands/GitlabCommands.php
+++ src/Blt/Plugin/Commands/GitlabCommands.php
@@ -4,33 +4,31 @@ namespace Acquia\GitlabPipelines\Blt\Plugin\Commands;
 
 use Acquia\Blt\Robo\BltTasks;
 use Acquia\Blt\Robo\Exceptions\BltException;
-use Acquia\Blt\Robo\Common\YamlWriter;
-use Acquia\Blt\Robo\Contract\VerbosityThresholdInterface;
-use Acquia\Blt\Robo\Commands\Recipes\CiCommand;
+use Robo\Contract\VerbosityThresholdInterface;
 
 /**
  * Defines commands related to Gitlab.
  */
 class GitlabCommands extends BltTasks {
 
-  /**
-  * Initializes default gitlab configuration for this project.
-  *
-  * @command recipes:ci:gitlab:init
-  *
-  * @aliases rcgc ci:gitlab:init
-  */
-  public function gitlabInit() {
-    $result = $this->taskFilesystemStack()
-      ->copy($this->getConfigValue('blt.root') . '/vendor/flux423/blt-gitlab-pipelines/.gitlab-ci.yml', $this->getConfigValue('repo.root') . '/.gitlab-ci.yml', TRUE)
-      ->stopOnFail()
-      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-      ->run();
+    /**
+     * Initializes default Gitlab Pipelines configuration for this project.
+     *
+     * @command recipes:ci:gitlab:init
+     * @throws \Acquia\Blt\Robo\Exceptions\BltException
+     */
+    public function azureInit() {
+        $result = $this->taskFilesystemStack()
+            ->copy($this->getConfigValue('repo.root') . '/vendor/flux423/blt-gitlab-pipelines/.gitlab-ci.yml', $this->getConfigValue('repo.root') . '/.gitlab-ci.yml', TRUE)
+            ->stopOnFail()
+            ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+            ->run();
 
-    if (!$result->wasSuccessful()) {
-      throw new BltException("Could not initialize Gitlab CI configuration.");
+        if (!$result->wasSuccessful()) {
+            throw new BltException("Could not initialize Gitlab configuration.");
+        }
+
+        $this->say("<info>A pre-configured Gitlab Pipelines file was copied to your repository root.</info>");
     }
 
-    $this->say("<info>A pre-configured .gitlab-ci.yml file was copied to your repository root.</info>");
-  }
 }
