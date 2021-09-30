2e7feb4066532ae94d08ba447583c8d2e8506c4e
Thomas P. Scola Jr <admin@423interactive.com>
2021-09-30 15:31:51 -0400

    Fix gitlab ci file again

--- scripts/gitlab/setup_env.sh
+++ scripts/gitlab/setup_env.sh
@@ -34,9 +34,10 @@ rm google-chrome-stable_current_amd64.deb
 # Create ssh-auth to host
 apt-get update -y && apt-get install openssh-client -y
 eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
-mkdir -p ~/.ssh && rm -rf ~/.ssh/known_hosts && touch ~/.ssh/known_hosts
-echo "$SSH_HOST" >> ~/.ssh/known_hosts
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+mkdir -p ~/.ssh
+rm -rf ~/.ssh/known_hosts
+ssh-keyscan -H "$SSH_HOST" >> ~/.ssh/known_hosts
 ssh-keyscan "$SSH_HOST" | sort -u - ~/.ssh/known_hosts -o ~/.ssh/known_hosts
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
